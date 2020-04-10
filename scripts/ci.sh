99d750056e2507f5690da9507f8a284aa74312e0
Rémi Passmoilesel <r.passmoilesel@protonmail.com>
2020-04-10 16:47:06 +0200

    Improve ci script

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,17 +2,26 @@
 
 set -e
 
-echo "Format, build, tests"
+echo ""
+echo "Go format, build, tests"
+echo ""
+
 go fmt
 go build
 go test
 
+echo ""
 echo "Web client build"
+echo ""
+
 cd web-client
 yarn install
 yarn build
 
+echo ""
 echo "Packaging"
+echo ""
+
 cd ..
 tar -cvf web-client.tar web-client/dist
 # TODO: package app with web client
